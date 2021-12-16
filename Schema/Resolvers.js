010c99f8a77770e8d26d5832fb9d4eb120d48ef5
mananmehta22 <manan.mehta@sts.in>
2021-12-16 18:17:02 +0530

    update query working

--- Schema/Resolvers.js
+++ Schema/Resolvers.js
@@ -31,8 +31,27 @@ const resolvers = {
           }
           }
         return result;
-        },
+        }, 
     },
+    Mutation: {
+      createEmployee(parent, args) {
+      const newEmployee = args;
+      employees.push(newEmployee);
+      return newEmployee;
+    },
+      updateEmployee(_, { first_name, last_name, birth_day, gender, emp_id, new_hire }) { 
+        const emp = employees.find(_employee_id => emp_id ); 
+        if (!emp) {
+          throw new Error(`Couldn't find employee with id ${emp_id}`);
+        }
+        emp.first_name = first_name; 
+        emp.last_name = last_name;
+        emp.birth_day = birth_day;
+        emp.gender = gender;
+        emp.new_hire = new_hire;
+        return emp;
+}
+    }
 };
 
 module.exports = { customScalarResolver }
--- Schema/TypeDefs.js
+++ Schema/TypeDefs.js
@@ -21,6 +21,11 @@ const typeDefs = gql`
   type Query {
     getAllEmployees: [Employee!]!
     getEmpID(ids: [Int]): [Employee]!
+  },
+  #Mutation
+  type Mutation {
+    createEmployee(first_name: String!, last_name:String!, birth_day: Date!, gender: Gender!, employee_id: ID!, new_hire:Boolean!): Employee!
+    updateEmployee(first_name: String!, last_name:String!, birth_day: Date!, gender: Gender!, emp_id: ID!, new_hire:Boolean!): Employee
   }
 `;
 
