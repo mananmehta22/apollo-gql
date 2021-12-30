5c39f66994e0e1e696ecc973b4a4bcad7d53da8b
mananmehta22 <manan.mehta@sts.in>
2021-12-30 23:57:00 +0530

    insert value

--- dbService.js
+++ dbService.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const { employees } = require("./EmployeeData");
 const dotenv = require('dotenv').config();
 
 const con = mysql.createConnection({
@@ -12,14 +13,21 @@ const con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = `CREATE TABLE employees (first_name VARCHAR(50) NOT NULL,
+    /* var sql = `CREATE TABLE employees (first_name VARCHAR(50) NOT NULL,
             last_name VARCHAR(50), birth_day DATE NOT NULL, 
             gender ENUM('Male','Female') NOT NULL, 
             employee_id INT AUTO_INCREMENT NOT NULL, new_hire BOOLEAN NOT NULL,
             PRIMARY KEY (employee_id)); `;
-  con.query(sql, function (err, result) {
+    con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
+    }); */
+    var sql1 = `INSERT INTO customers (first_name, last_name, 
+            birth_day, gender, 
+            employee_id, new_hire) VALUES ?`;
+            con.query(sql1, employees, function (err, result) {
+                if (err) throw err;
+                console.log("Number of records inserted: " + result.affectedRows);
   });
   });
 
