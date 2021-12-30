4ea7a9e609e17d02484a3c6d020aa1271cc42365
mananmehta22 <manan.mehta@sts.in>
2021-12-30 20:49:21 +0530

    added db connection and table

---
+++ dbService.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+const dotenv = require('dotenv').config();
+
+const con = mysql.createConnection({
+  host: process.env.HOST,
+  port: process.env.DB_PORT,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = `CREATE TABLE employees (first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50), birth_day DATE NOT NULL, 
+            gender ENUM('Male','Female') NOT NULL, 
+            employee_id INT AUTO_INCREMENT NOT NULL, new_hire BOOLEAN NOT NULL,
+            PRIMARY KEY (employee_id)); `;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+  });
+
